fac4ec78196ac00519ba7bb29122895b5c9f7327
Harsha-Bhattacharyya <somnathbhattacharyya510@gmail.com>
2024-12-12 23:29:33 +0530

    Update Lexer.cpp

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,12 +1,12 @@
-/*This program is free software: you can redistribute it and/or modify it
+/*The CppScript Lexer is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the 
 Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, 
+The CppScript Lexer is distributed in the hope that it will be useful, 
 but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with this program.
+You should have received a copy of the GNU General Public License along with the CppScript Lexer.
 If not, see <https://www.gnu.org/licenses/>.*/
 
 
